96d690f536a160a6356b3c0b6cd4ce7854316133
john-nath <jadikwu22@gmail.com>
2024-11-26 12:37:50 +0100

    third commit

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodeResolve()],
   server: {port: 5176},
   build: {
     rollupOptions: {
-      external: ['react-router-dom']
+      external: ["react-router-dom", "Routes", "Route", /node_modules/]
     }
   }
 })
