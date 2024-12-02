385687666ce14fdb5a9d98d7ca60eb881846b5a1
john-nath <jadikwu22@gmail.com>
2024-12-02 07:50:16 +0100

    rand

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), nodeResolve()],
+  plugins: [react()],
   server: {port: 5176},
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom", "react-toastify", "Routes", "Route", "ToastContainer", /node_modules/]
-    }
-  }
 })
