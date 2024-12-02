9535aebace73a9430176ef8392d223ef49c30bd3
john-nath <jadikwu22@gmail.com>
2024-12-02 15:53:10 +0100

    rand

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import {Routes, Route} from 'react-router-dom'
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import {Routes, Route} from 'react-router-dom'
+// import { ToastContainer} from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Add from './pages/Add'
