be1c0e35a7c3fb354ed7fc72e87a20b579d47f8f
john-nath <jadikwu22@gmail.com>
2024-12-02 15:57:07 +0100

    rand

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { assets } from '../assets/assets'
 import {backendUrl} from '../App'
 import { toast } from 'react-toastify'
