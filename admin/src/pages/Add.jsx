06b7378557872eeb39d624926b94069965b4d9cd
john-nath <jadikwu22@gmail.com>
2024-12-02 16:05:11 +0100

    rand

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 // import axios from 'axios'
 import { assets } from '../assets/assets'
 import {backendUrl} from '../App'
-import { toast } from 'react-toastify'
+// import { toast } from 'react-toastify'
 
 const Add = ({token}) => {
   const [image1,setImage1] = useState(false)
